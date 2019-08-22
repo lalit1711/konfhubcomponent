41612cb8bcf359cea9841b3f4f649113c176db0f
lalittemm7707 <lalit.sahrma.ls45@gmail.com>
2019-08-22 18:09:42 +0530

    Added auto complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, {useState} from 'react';
 import InputBox from './components/InputBox';
+import AutoComplete from './components/AutoComplete';
 import './App.css';
 import 'bulma/css/bulma.min.css'
 import 'font-awesome/css/font-awesome.min.css';
+const data = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"]
 function App() {
   const [loading, changeLoading] = useState("");
   const changeLoadState = () => {
@@ -12,13 +14,13 @@ function App() {
    };
   return (
     <div>
-      <InputBox type="email"  name="email" leftIcon="envelope" loading={loading}  rightIcon="check" />
-      <button className="btn btn-primary" onClick={e => {changeLoadState()}}>Loading</button>
+      <AutoComplete value="" data={data} leftIcon="search" placeholder="Autocomplete"  />
+      <InputBox type="email" name="Enter Email" isRequired={true} case="none" value="" max_length={100} min_length={2} rightIcon="check" />
     </div>
   );
 }
 
-// <InputBox type="email" name="Enter Email" isRequired={true} case="none" value="" max_length={100} min_length={2} rightIcon="check" />
+
 //       <InputBox type="text" name="Name" isRequired={true} case="none" value="" max_length={10} min_length={2} loading={loading} leftIcon="envelope" rightIcon="check" />
 //       <button className="btn btn-primary" onClick={e => {changeLoadState()}}>Loading</button>
 
---
+++ src/components/AutoComplete.js
@@ -0,0 +1,118 @@
+import React,{useState} from 'react';
+import InputBox from './InputBox';
+
+const AutoComplete = (props) => {
+    const[value, setValue] = useState(props.value);
+    const[AutoComp, setAutoComp] = useState("true")
+    const _hasLeftIcon = props.leftIcon !== undefined ? " has-icons-left" : "";
+    const _hasRightIcon = props.rightIcon !== undefined ? props.loading === "" || props.loading === undefined ? " has-icons-right" : "" : "";
+    const _isLoading = props.loading !== undefined ? props.loading : "";
+    const changeTheValue = (v) => {
+        if(v.trim()!== ""){
+            setAutoComp("true");
+            setValue(v);
+        }else{
+            setValue("");
+        }
+    }
+
+    const setDataFromChild = (v) => {
+        setAutoComp("false");
+        setValue(v);
+    }
+
+    return(
+        <nav className="">
+        <div className="">
+            <div className={"control " + _isLoading + _hasLeftIcon + _hasRightIcon }>
+            <input value={value} className="input" type="text" placeholder={props.placeholder} onChange={e => {changeTheValue(e.target.value)}} />
+            {
+                (()=> {
+                    if(props.leftIcon !== undefined){
+                        return(
+                            <LeftIcon font={props.leftIcon} />
+                        )
+                    }
+                })()
+            }{
+                (() => {
+                    if(props.rightIcon !== undefined){
+                        return(
+                            <RightIcon font={props.rightIcon} />
+                        )
+                    }
+                })()
+            }
+            </div>
+        </div>
+            <SearchElement keytearm={value} setValue = {setDataFromChild} autocom={AutoComp} data={props.data} />
+        </nav>
+    )
+};
+
+const LeftIcon = (props) => {
+    return(
+        <span className="icon is-small is-left">
+            <i className={"fa fa-"+props.font}></i>
+        </span>
+    )
+}
+
+const RightIcon = (props) => {
+    return(
+            <span className="icon is-small is-right">
+                <i className={"fa fa-"+props.font}></i>
+            </span>
+    )
+}
+
+
+const SearchElement = (props) => {
+    const data=props.data
+    let _data = [];
+    const _key = props.autocom === "true" ? props.keytearm : "";
+    if(_key!== ""){
+        _data = data.filter(v=> {
+            return v.toLowerCase().indexOf(_key.toLowerCase()) > -1
+        });
+    }else{
+        _data = []
+    }
+    const setValue = (v) => {
+        _data = [];
+        props.setValue(v);
+    }
+    if(_data.length){
+        return(
+            <div style={styles.auto}>
+            {
+                _data.map((v,i) => {
+                    return(
+                        <span key={i}  className="panel-block is-active" onClick ={e => {setValue(v)}}>
+                            {v}
+                        </span>
+                    )
+                })
+            }
+            </div>
+        )
+    }
+    else{
+        return(
+            <div></div>
+        )
+    }
+};
+
+const styles = {
+    auto:{
+        overflow: "auto",
+        "zIndex": 1000,
+        "background": "white",
+        "position": "fixed",
+        "width": "100%",
+        "paddingHorizontal" : "13px"
+    }
+}
+
+export default AutoComplete;
